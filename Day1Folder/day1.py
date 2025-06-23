12edca3808c87764b0b1af59888d4c3639d21df7
qitaicanabilistic <qi88tai@gmail.com>
2025-06-23 03:12:36 +0000

    Jun 23, 2025, 3:12 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,29 +1,43 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
+# print("hello from day1")
 
 ########################################################################
 # Task 1:
-
+# print("my name is Qitai")
+# print("computhink")
 
 
 ########################################################################
 # Task 2:
-
+# print("if I had $2000, I will put it in my bank account and invest it later, when I am older ")
 
 
 ########################################################################
 # Task 3:
+# wallet=10000000
+# wallet=10000000+3*2-10/2
+# print(wallet)
 
-
-
+# animal1="tiger"
+# animal2="snake"
+# animal3="hamster"
+# print(animal1)
+# print(animal2)
+# print(animal3)
 ########################################################################
 # Task 4:
-
-
-
+variable1=45
+variable2=24
+print(variable1+variable2)
+print(variable1*variable2)
 ########################################################################
 # Task 5:
+name="cannaibal"
+food="meat"
+print(name +" says I like "+ food)
+print("My name is "+  name)
+print("I like to eat "+  food)
 
 
 
