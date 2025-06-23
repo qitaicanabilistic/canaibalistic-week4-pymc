b8c4e1a37675f60f2ea24a36b6c106a9c9073bf8
qitaicanabilistic <qi88tai@gmail.com>
2025-06-23 03:31:08 +0000

    Jun 23, 2025, 3:31 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -50,6 +50,15 @@ def greetings():
 # step 2
 greetings()
 
+def intro():
+    print("my name is Qitai")
+    print("computhink")
+    print("if I had $2000, I will put it in my bank account and invest it later, when I am older ")
+
+
+
+    
+
 
 ########################################################################
 # Task 7:
